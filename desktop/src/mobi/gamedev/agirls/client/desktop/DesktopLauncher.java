e8c65cecdd00e2f0cc238c6496e655fc93a564b2
Denis <malzoff2003@gmail.com>
2017-08-30 13:47:03 +0700

    init

---
+++ desktop/src/mobi/gamedev/agirls/client/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mobi.gamedev.agirls.client.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mobi.gamedev.agirls.client.AgirlsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AgirlsGame(), config);
+	}
+}
